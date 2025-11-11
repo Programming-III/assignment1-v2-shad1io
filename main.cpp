4509f3b28d3a790d13aa6b998e35070ef7f16730
Shady Ashraf <shady.kabil@student.giu-uni.de>
2025-11-11 09:45:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "Student.h"
 #include "Instructor.h"
 #include "Course.h"
+#include "Instructer.h"
 
 
 using namespace std;
 
 // ==================== Person Class Implementation =========================
-
 Person::Person(){}
 Person::Person(string name,int id) {
     this->name = name;
@@ -36,9 +36,29 @@ void Person::display() {
 
 
 
-
-
 // ==================== Student Class Implementation ====================
+
+Student::Student(){}
+Student::Student(int yearLevel,string major){
+    this->yearLevel = yearLevel;
+    this->yearLevel = yearLevel;
+}
+string Student::getMajor() {
+    return major;
+}
+void Student::setMajor() {
+    this-> major = major;
+}
+int Student::getYearlevel() {
+    return yearLevel;
+}
+void Student::setYearLevel() {
+    this -> yearLevel = yearLevel;
+}
+void Student::display() {
+    cout << "Year: "<< yearLevel <<endl;
+    cout << "Major: " <<major <<endl;
+}
 
 
 
@@ -46,15 +66,37 @@ void Person::display() {
 
 
 // ==================== Instructor Class Implementation ====================
+Instructer::Instructer() {
 
+}
+Instructer::Instructer(string department,int experienceYears) {
+    this -> department = department;
+    this -> experienceYears = experienceYears;
+}
+string Instructer::getDepartment() {
+    return department;
+}
+int Instructer::getExperienceYears() {
+    return experienceYears;
+}
+void Instructer::setDepartment(string department) {
+    this -> department = department;
 
-
+}
+void Instructer::setExperienceYears(int experienceYears){
+this -> experienceYears = experienceYears;
+}
+void Instructer::display() {
+    cout << "Department: "<< department <<endl;
+    cout << "Experience: "<< experienceYears <<" Years"<<endl;
+}
 
 
 
 // ==================== Course Class Implementation ====================
 
-
+Course::Course(){}
+Course::Course()
 
 
 
@@ -63,7 +105,7 @@ void Person::display() {
 
 // ==================== Main Function ====================
 int main() {
-   
-    
+
+
     return 0;
 }
